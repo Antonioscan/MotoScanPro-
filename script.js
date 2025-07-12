a6c500faa577c01b4e5b9ba2eb1c7bffb79df6b2
Antonioscan <mustang3@live.it>
2025-07-12 20:36:33 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".btn-blue").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    alert("Funzione in sviluppo!");
+  });
+});
+
+document.querySelector(".btn-ebay").addEventListener("click", () => {
+  alert("Creazione inserzione eBay in sviluppo!");
+});
