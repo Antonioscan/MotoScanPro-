7bbd25e47d3e4b4f23f40e0760694c360504bb67
Antonioscan <mustang3@live.it>
2025-07-12 20:55:01 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".btn-blue").forEach((btn) => {
-  btn.addEventListener("click", () => {
-    alert("Funzione in sviluppo!");
-  });
-});
-
-document.querySelector(".btn-ebay").addEventListener("click", () => {
-  alert("Creazione inserzione eBay in sviluppo!");
-});
